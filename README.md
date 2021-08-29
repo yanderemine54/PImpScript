8eae5fa6fac3aae707ddbf11b7b0c25dd9a77b41
Yanderemine54 <ymtechnologies@mail.com>
2021-08-29 14:43:39 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PImpScript
+
+## Installing
+
+### On *NIX
+Clone the repository:
+
+`git clone https://github.com/yanderemine54/PImpScript.git`
+
+## FAQ
+__Q: Why PImpScript__
+
+A: PImpScript stands for "Procedural Imperative Scripting Language" and aims to be a simple yet powerful programming language. From no coding experience to senior software developer, PimpScript got you covered!
---
+++ repl.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void eval(const char*);
+
+int main(int argc, char const *argv[])
+{
+    char expr[128];
+
+    puts("Welcome to PImpScript!");
+    printf("> ");
+    fgets(expr, 128, stdin);
+    eval(expr);
+    
+    return 0;
+}
+
+void eval(const char *statement)
+{
+    char *token;
+    const char *delim = " ";
+    token = strtok(statement, delim);
+    while (token != NULL)
+    {
+        printf("Token: %s\n", token);
+
+        token = strtok(NULL, delim);
+    }
+}
